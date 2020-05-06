ca6767b11025db63012578b0498dd4afeeb7db06
Rafael <rafaelmp@gmail.com>
2020-05-06 02:43:02 -0500

    (fix) Precios

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,7 +10,7 @@ const Product = ({product}) => {
         <img className="card-img-top" src={product.IMAGE} alt={`Imagen de ${product.NAME}`} />
 
         <div className="card-body">
-
+          <h2>Precio: $ {product.PRICE}</h2>
         </div>
       </div>
     </div>
